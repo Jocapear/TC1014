1dbd0d7a2f61e66d7c9f04eeeb3e34c4de76a752
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2015-08-28 09:09:50 -0500

    Create WSQ03.py

---
+++ WSQ03.py
@@ -0,0 +1,20 @@
+x = input("Give a number")
+x = int(x)
+y = input("Give another number")
+y = int(y)
+op1 = x + y
+op2 = x - y
+op3 = x * y
+op4 = x / y
+op5 = x % y
+x = str(x)
+y = str(y)
+op1 = str(op1)
+op2 = str(op2)
+op3 = str(op3)
+op4 = str(op4)
+print("The sum of ",x," and ",y," is ",op1)
+print(x+" minus ",y," is ",op2)
+print("The product of ",x," and ",y," is ",op3)
+print("The division of ",x," and ",y," is ",op4)
+print("The remainder of ",x," and ",y," is ",op5)
